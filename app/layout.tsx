de86de748f71156f2495a386b429b875584f02ec
arpitnexuses <arpit.m@nexuses.in>
2025-01-30 15:01:45 +0530

    5th message

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Exa Dashboard',
-  description: 'Beautiful frontend for Exa.ai',
+  title: 'Nex Dashboard',
+  description: 'Beautiful frontend for test',
 }
 
 export default function RootLayout({
